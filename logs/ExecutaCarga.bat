c6ebd1be491bde370d7933b4c7f7924077f3481f
William Mian <william_mian@yahoo.com.br>
2021-06-16 16:13:26 -0300

    etl

---
+++ logs/ExecutaCarga.bat
@@ -0,0 +1,4 @@
+c:
+cd\
+cd C:\treinamento\designer-tools\data-integration
+kitchen.bat /rep:reposucos /job:jobPrincipal /dir: /level:Basic /log:"C:\treinamento\logs\jobLog.log" > "C:\treinamento\logs\consoleLog.log"
